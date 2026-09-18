f5162f37e937c39883096eb7c5f3810239ea7938
Samuel <ssimon7@msudenver.edu>
2026-09-18 10:06:28 -0600

    add team names to file headers and fill in pyproject

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,7 +1,7 @@
 '''
 CS3250 - Software Development Methods and Tools
 Instructor: Thyago Mota
-Student: 
+Student: Samuel Simon, Lucas Roque, Sagun Shrestha
 Description: Project 1 - GPA Calculator
 '''
 
