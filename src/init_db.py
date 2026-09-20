adc6aed1183b31757a88c2497b38252645e31d68
Samuel <ssimon7@msudenver.edu>
2026-09-20 17:55:50 -0600

    load initial courses in init_db

--- src/init_db.py
+++ src/init_db.py
@@ -10,10 +10,17 @@ from app.models import Course
 
 # TODO
 courses = [
-    
+   ('CS', '3250', 'Software Development Methods and Tools', 4),
+   ('NAP', '1010', 'Introduction to Napping', 3),
+   ('DOG', '2200', 'Advanced Belly Rubs', 4),
+   ('GIT', '4040', 'Merge Conflicts and Crying', 3),
+   ('MEME', '3000', 'History of Bop It Commercials', 2),
+   ('PROC', '1999', 'Procrastination Theory', 1) #which we can finish later, maybe
 ]
 
 with app.app_context():
     for prefix, number, name, credits in courses:
-        pass
+        course = Course(prefix=prefix, number=number, name=name, credits=credits)
+        db.session.add(course)
+    db.session.commit()
     print(f'Loaded {len(courses)} courses.')
